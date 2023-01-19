7cb178ba1ae93a91a68af8ff7d6ad15966b986b5
leejung <dmswjd9751@naver.com>
2023-01-20 01:17:49 +0900

    HTML : revise

---
+++ main.js
@@ -0,0 +1,8 @@
+const toogleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const util = document.querySelector('.navbar_util');
+
+toogleBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('active');
+    util.classList.toggle('active');
+});
